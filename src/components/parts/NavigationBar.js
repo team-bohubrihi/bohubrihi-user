55d12ef82f5eac636e8e70c6c4433e06952d5798
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-05-25 19:12:47 +0600

    Create Navbar Component

---
+++ src/components/parts/NavigationBar.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../../assets/css/NavigationBar.css';
+import logo from '../../assets/images/Bohubrihi-Online-Course-Logo-Updated-White.png';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+} from 'reactstrap';
+
+const NavigationBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <Navbar dark expand="md" className="deep-blue-background">
+            <div className="container">
+                <Link className="navbar-brand" to="/"><img src={logo} alt="Bohubrihi Logo" /></Link>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="ms-auto mb-2 mb-lg-0" navbar>
+
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                Courses
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>
+                                    Career Tract Program
+                            </DropdownItem>
+                                <DropdownItem>
+                                    Course Category
+                            </DropdownItem>
+
+                                <DropdownItem>
+                                    Free Course
+                            </DropdownItem>
+
+                                <DropdownItem divider />
+                                <DropdownItem>
+                                    All Courses
+                            </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+
+                        <NavItem>
+                            <NavLink exact to="/" activeClassName="yellow" className="nav-link">Corporate e-learning</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </div>
+        </Navbar>
+    );
+};
+
+export default NavigationBar;
