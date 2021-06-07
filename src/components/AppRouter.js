e5b4106663b0f5a05284e1bbd48408f2954240ba
Md. Rifat Islam <71067300+xxxxxxRIFATxxxxxx@users.noreply.github.com>
2021-06-07 10:41:56 +0600

    Course Details Page Upper Section Design Complete (#6)
    Co-authored-by: simanta-bohubrihi <82964739+simanta-bohubrihi@users.noreply.github.com>

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -2,6 +2,7 @@ import Home from './pages/Home';
 import Courses from './pages/Courses';
 import CourseLearn from './pages/CourseLearn'
 import Dashboard from './pages/Dashboard';
+import CourseDetails from './pages/CourseDetails';
 import { Switch, Route } from 'react-router-dom';
 
 const AppRouter = () => {
@@ -10,8 +11,9 @@ const AppRouter = () => {
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/courses" exact component={Courses} />
-                <Route path="/course/learn/modern-javascript" exact component={CourseLearn} />
                 <Route path="/dashboard" exact component={Dashboard} />
+                <Route path="/courseDetails" exact component={CourseDetails} />
+                <Route path="/course/learn/modern-javascript" exact component={CourseLearn} />
             </Switch>
         </div>
     )
